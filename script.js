d5f8c88f4eb65414ecc5a58c27582bc5fd442468
Ronnie Bunao <ronniebunao001@gmail.com>
2023-07-12 00:31:50 +0800

    Add JS function for library and table

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
 let library = [];
 
-function Book(title,authors,pages,readInfo){
+function Book(title,authors,pages,read){
     this.title = title;
     this.authors = authors;
     this.pages = pages;
-    this.readInfo = readInfo;
+    this.read = read;
 }
 
-Book.prototype.info = function(){
-    return `${this.title} by ${this.authors}, ${this.pages} pages, ${this.readInfo}`
-}
-
-
-function addBook(){
-    const book1 = new Book("DD1", "Abel", 999, "Read");
-    library.push(book1)
-}
-
-const addBookBtn = document.querySelector('.btn');
-addBookBtn.addEventListener('click',() =>{
-    addBook();
+let addBookBtn = document.querySelector('.btn');
+addBookBtn.addEventListener('click',function(){
+    let bookForm = document.querySelector('#bookForm');
+    bookForm.style.display = 'block'
+    console.log(bookForm)
 })
-function showLibrary(){
-    const tableBody = document.querySelector('.tableBody')
-    for(let item of library){
-        const row = document.createElement('tr')
-        for(let key in item){
-            if(item.hasOwnProperty(key)){
-                const cell = document.createElement('td');
-                cell.textContent = item[key];
-                row.appendChild(cell);
-            }
-        }
-        tableBody.appendChild(row);
-    }
+function addBookToLibrary(){
+    let title = document.querySelector("#title").value;
+    let authors = document.querySelector("#authors").value;
+    let pages = document.querySelector("#pages").value;
+    let read = document.querySelector("#read").value;
+
+    const newBook = new Book(title,authors,pages,read);
+    library.push(newBook)
+    console.log(library)
 }
+document.querySelector('#bookForm').addEventListener('submit',function(event){
+    event.preventDefault();
+    addBookToLibrary()
+    showLibrary()
+})
+
+function showLibrary() {
+    const tableBody = document.querySelector('.tableBody');
+    const lastItem = library[library.length - 1]; // Retrieve the last element
+  
+    const row = document.createElement('tr');
+    for (let key in lastItem) {
+      if (lastItem.hasOwnProperty(key)) {
+        const cell = document.createElement('td');
+        cell.textContent = lastItem[key];
+        row.appendChild(cell);
+      }
+    }
+    tableBody.appendChild(row);
+  }
+  
