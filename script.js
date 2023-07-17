b2690bd31c13cb21f7c5eccc0b97b4d159026d52
Ronnie Bunao <ronniebunao001@gmail.com>
2023-07-17 19:08:02 +0800

    Change constructor to class
    -add delete button
    -change readInfo to a button
    -change message of readInfo to yes or no

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
 let library = [];
-
-function Book(title,authors,pages,read){
-    this.title = title;
-    this.authors = authors;
-    this.pages = pages;
-    this.read = read;
-}
-
 let addBookBtn = document.querySelector('.btn');
 addBookBtn.addEventListener('click',function(){
     let bookForm = document.querySelector('#bookForm');
     bookForm.style.display = 'block'
-    console.log(bookForm)
 })
-function addBookToLibrary(){
+
+document.querySelector('#bookForm').addEventListener('submit',function(event){
+    event.preventDefault();
+    Book.addBookToLibrary()
+    Book.showLibrary()
+})
+
+
+
+class Book{
+  constructor(title,authors,pages,read){
+    this.title = title;
+    this.authors = authors;
+    this.pages = pages;
+    this.read = read;
+  }
+  
+  static addBookToLibrary(){
     let title = document.querySelector("#title").value;
     let authors = document.querySelector("#authors").value;
     let pages = document.querySelector("#pages").value;
-    let read = document.querySelector("#read").value;
+    let readCheckbox = document.querySelector("#read");
+    let read = readCheckbox.checked ? 'Yes' : 'No';
 
     const newBook = new Book(title,authors,pages,read);
     library.push(newBook)
-    console.log(library)
-}
-document.querySelector('#bookForm').addEventListener('submit',function(event){
-    event.preventDefault();
-    addBookToLibrary()
-    showLibrary()
-})
+  }
 
-function showLibrary() {
-    const tableBody = document.querySelector('.tableBody');
-    const lastItem = library[library.length - 1]; // Retrieve the last element
   
+
+  static showLibrary() {
+    const tableBody = document.querySelector('.tableBody');
+    const lastItem = library[library.length - 1]; 
+
     const row = document.createElement('tr');
     for (let key in lastItem) {
       if (lastItem.hasOwnProperty(key)) {
@@ -41,6 +46,28 @@ function showLibrary() {
         row.appendChild(cell);
       }
     }
+
+    const deleteCell = document.createElement('td');
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Del'
+    deleteButton.addEventListener('click', ()=>{
+      const rowIndex = Array.from(tableBody.children).indexOf(row);
+      library.splice(rowIndex, 1)
+      tableBody.removeChild(row);
+    });
+
+    deleteCell.appendChild(deleteButton);
+    row.appendChild(deleteCell)
+
     tableBody.appendChild(row);
   }
+}
+
+
+
+
+
+
+
+
   
